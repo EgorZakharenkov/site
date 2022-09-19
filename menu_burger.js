64b595e1a7bd798f6114b1c5cb6aa276bb64a2b7
EgorZakharenkov <93191584+EgorZakharenkov@users.noreply.github.com>
2022-09-19 16:37:10 +0300

    Delete menu_burger.js

--- menu_burger.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.header_burger');
-const menu = document.querySelector('.header_nav');
-
-button.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-})
